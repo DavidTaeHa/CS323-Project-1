20c3012d6e9bb0809c9da81aa19754ce0d39bbb6
Gandhi115 <61490541+Gandhi115@users.noreply.github.com>
2022-04-10 21:48:33 -0400

    update

--- Newton Horners/newton_horner.m
+++ Newton Horners/newton_horner.m
@@ -1,20 +1,35 @@
 %Newton with horners algorithm
 %INPUTS: a0,a1,a2,...,an , x0 , E, N
 clear
+% Get User Input
+prompt = "What is the file name of your test case?";
+
+% Stores the name of the txt file
+txtFile = input(prompt, "s");
+
+% Opens the txt file
+openFile = fopen(txtFile); 
+
+% Gets all the necessary stuff from the txt file
+%variables = fscanf(openFile,'%f');
+variables = fscanf(openFile,'%f');
+
+% Closes the file
+fclose(openFile);
+
 list = []; %list of coefficients of polynomial
-degree = input("Input polynomial degree:\n");
-fprintf("Input coefficients:\n");
+degree = variables(1)
 n = degree + 1; %number of coefficients in polynomial
 
 %Adds input coefficients to an array
-for i=1:n
-    temp = input("");
+for i=2:(n+1)
+    temp = variables(i)
     list = [list, temp];
 end
 
-x0 = input("Input x0:\n");
-E = input("Input error tolerance:\n"); %error tolerance
-N = input("Input max number\n"); %max number of inputs
+x0 = variables(end-2)
+E = variables(end-1) %error tolerance
+N = variables(end) %max number of inputs
 e = 100; %current error
 i = 0; %counter for iterations
 
